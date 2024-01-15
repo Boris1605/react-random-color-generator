58db0e0f3df635858889cd04e6cd8a58fc3d8dd0
Boris Djuric <boris_djuric@hotmail.com>
2024-01-15 21:57:42 +0100

    Add some text to readme, add style to button and div

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # REACT RANDOM COLOR GENERATOR
+
+This React web application generates and display random colors when a button is clicked. The application consists of a button with the text "Generate" and a div that displays the generated color with its hex code.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 
 export default function App() {
+  // State hook to manage the color state with initial value '#000000'
   const [color, setColor] = useState('#000000');
+  // Function to generate a random color and update the state
   const generateColor = () => {
+    // Generating a random hexadecimal color code
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // Updating the color state with the generated color
     setColor(randomColor);
   };
 
@@ -12,14 +16,27 @@ export default function App() {
       <h1>Random Color Generator</h1>
       <div>
         <div
-          style={{ backgroundColor: color, height: '100px', width: '200px' }}
+          style={{
+            backgroundColor: color,
+            height: '100px',
+            width: '200px',
+            borderRadius: '10px',
+            fontSize: '16px',
+            textAlign: 'center',
+            paddingTop: '10px',
+          }}
         >
           Generated Color: {color}
         </div>
         <br />
         <button
           onClick={generateColor}
-          style={{ borderRadius: '50px', fontSize: '20px', width: '200px' }}
+          style={{
+            borderRadius: '12px',
+            fontSize: '20px',
+            width: '200px',
+            textAlign: 'center',
+          }}
         >
           Generate
         </button>
