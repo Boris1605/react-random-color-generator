18803725edf014b5a24728b540e760403f3d5d66
Boris Djuric <boris_djuric@hotmail.com>
2024-01-15 16:19:51 +0100

    Add code to generate random color, add style to button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import React, { useState } from 'react';
+
 export default function App() {
-  return <div>HELLO</div>;
+  const [color, setColor] = useState('#000000');
+  const generateColor = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+  };
+
+  return (
+    <>
+      <h1>Random Color Generator</h1>
+      <div>
+        <div
+          style={{ backgroundColor: color, height: '100px', width: '200px' }}
+        >
+          Generated Color: {color}
+        </div>
+        <br />
+        <button
+          onClick={generateColor}
+          style={{ borderRadius: '50px', fontSize: '20px', width: '200px' }}
+        >
+          Generate
+        </button>
+      </div>
+    </>
+  );
 }
